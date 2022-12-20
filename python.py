612155779c5dd680a5274657ba9f9812b6f920bf
“monique.jackson” <bymoniquejackson@gmail.com>
2022-12-20 12:54:23 +0000

    trying to commit

---
+++ python.py
@@ -0,0 +1,114 @@
+# print("hello world")
+# name, age, colour = "Momo", "25", "blue"
+# print(name)
+# print(age)
+# print(colour)
+#
+#
+# name = "Momo"
+#
+# age = "25"
+#
+# fav_colour = "blue"
+#
+# print(f"my name is {name} I am {age} and my favourite colour is {fav_colour}.")
+#
+#
+#
+#
+#
+#
+#
+# a = int(6)
+# b = float(2)
+# c = str("hello")
+# print (type(a), type(b), type (c))
+#
+#
+
+
+
+# a, b, c = "cat", "dog", "mouse"
+#
+# print(b)
+
+
+#
+# websites = [
+#     "Twitch",
+#     "Goodreads",
+#     "Notion",
+# ]
+#
+# countries = [
+#     "France",
+#     "Spain",
+#     "Germany"
+# ]
+#
+# # websites.extend(countries)
+# print(websites)
+
+
+
+
+
+#global variable
+
+# x = "hello"
+# def say_hello():
+#     print ("say ", x)
+#
+#     say_hello()
+
+
+
+
+
+# name, age, int = "Momo", "26", "5"
+# age + int
+# total = age + int
+# print(name)
+# print(age)
+# print(int)
+# print (total)
+#
+#
+# Name = "MJ"
+# Age = 26
+# random_num = 5
+# total = age + random_num
+# print ("age now: " + str(total))
+
+# my_name = "mj"
+# age = 26
+# sum = 50
+# total = age + sum
+# print( "age: " + str(total))
+
+
+
+# my_string = "hola el mundo"
+#
+# print("hola el mundo" [8])
+# print("hola el mundo"[0])
+# print("hola el mundo"[12])
+# print("The length of the string is:",len(my_string))
+# print (my_string [7:13])
+# print("It's a beatufiul day!" + my_string)
+
+
+# print("estio"[2])
+#
+# my_string = "Apprenticeship"
+# print(f'{my_string}\n'* 5)
+#
+# another_string = "I love python"
+# print ("the length of this string is:", len(another_string))
+#
+#
+# phrase = input ("Drink lots of water")
+# if phrase == "water":
+#     print
+
+    # print(10>20)
